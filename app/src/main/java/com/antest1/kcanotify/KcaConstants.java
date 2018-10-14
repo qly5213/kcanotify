9bcae0b960dcbf78c614955c78e83f14a767779e
qly5213 <qlyang1993926@gmail.com>
2018-10-14 15:27:27 +0900

    Update KcaConstants.java

--- app/src/main/java/com/antest1/kcanotify/KcaConstants.java
+++ app/src/main/java/com/antest1/kcanotify/KcaConstants.java
@@ -6,7 +6,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public final class KcaConstants {
-    public final static String KC_PACKAGE_NAME = "com.dmm.dmmlabo.kancolle";
+//    public final static String KC_PACKAGE_NAME = "com.dmm.dmmlabo.kancolle";
+    public final static String KC_PACKAGE_NAME = "com.akeno0810.kancolleHtml5Browser";
     public final static String DMMLOGIN_PACKAGE_NAME = "com.dmm.app.store";
     public final static String MAINACTIVITY_NAME = "com.antest1.kcanotify.MainActivity";
 
